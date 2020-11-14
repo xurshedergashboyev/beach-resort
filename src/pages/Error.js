ac492564b813e5fd14981f66e9850ea0cc749b4e
unknown <xurshedergashboyev@gmail.com>
2020-11-14 15:47:45 +0500

    Services component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 
 export default class Error extends Component {
     render() {
         return (
             <div>
-                Hello from Error page
+                <Hero>
+                    <Banner title="404" subtitle="page not found">
+                        <Link to='/' className="btn-primary">
+                            return home
+                        </Link>
+                    </Banner>
+                </Hero>
             </div>
         )
     }
